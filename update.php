c2ef3b4d2820039ce8fc14d9c54c0475c4b61343
frankSudchada <152998576+frankSudchada@users.noreply.github.com>
2023-12-19 21:53:11 +0700

    Add files via upload

---
+++ update.php
@@ -0,0 +1,4 @@
+{"Version":"1.1","Url":"https://github.com/FrankSudchada/FrankSudchada/raw/main/aa.zip","Text":"📢อัพเดท วันที่ 19 ธันวาคม 2566
+🤔ต้องการแจ้งปัญหาต่างๆ
+🤔ต้องการสอบถามรายละเอียด
+🤔กรุณาทักแชท"}
